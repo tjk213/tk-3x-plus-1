66698f3169876fab6dc436e1b860e794e04bb221
Tyler J. Kenney <tyler@modular.com>
2023-11-21 18:42:11 -0500

    Add support for --step-map-out

--- tk-3x+1.py
+++ tk-3x+1.py
@@ -43,6 +43,16 @@ def main():
     range_parser.add_argument("--start", type=int, metavar="<int>", required=True, help="Starting index.")
     range_parser.add_argument("--stop", type=int, metavar="<int>", default=None, help="Ending index. [Default: 2*start]")
 
+    step_map_parser = parser.add_argument_group(
+        title="Step Map Options", description="\n"
+        "Step maps are a data structure that point from any index to the number of steps in\n"
+        "the conjecture verification. Step maps enable one CC verification run to build on\n"
+        "the results from a prior run, which can, in some cases, lead to significant speedups.\n"
+        "Step maps are saved in CSV format."
+    )
+    step_map_parser.add_argument("--step-map-in", type=str, metavar="/path/to/step/map", help="Load given step map.")
+    step_map_parser.add_argument("--step-map-out",type=str, metavar="/path/to/step/map", help="Save results as step map.")
+
     args = parser.parse_args()
     stop = 2*args.start if not args.stop else args.stop
     start = args.start if args.start % 2 == 1 else args.start+1
@@ -50,8 +60,7 @@ def main():
     visited = set()
     overflows = set()
 
-    max_steps = 0
-    total_steps = 0
+    steps = dict()
     N = len(range(start, stop))
 
     tstart = time()
@@ -66,13 +75,20 @@ def main():
 
         if res['maxval'] >= 2**32:
             overflows.add(i)
-        total_steps += res['steps']
-        max_steps = max(max_steps, res['steps'])
+        steps[i] = res['steps']
     tend = time()
 
+    if args.step_map_out:
+        with open(args.step_map_out, 'w') as f:
+            print("index, steps", file=f)
+            pairs = list(sorted(steps.items()))
+            for p in pairs:
+                print(f"{p[0]}, {p[1]}", file=f)
 
     perc = len(overflows) / N * 100
+    total_steps = sum(steps.values())
     avg_steps = total_steps / N
+    max_steps = max(steps.values())
     checks_per_second = N / (tend - tstart)
 
     #print(f'Collatz Conjecture Confirmed.')
